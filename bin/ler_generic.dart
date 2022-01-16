87c139cfa77015b5eb8f328488439879d5e8b804
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-01-16 11:02:58 +0700

    add generice

---
+++ bin/ler_generic.dart
@@ -0,0 +1,22 @@
+class Brankas<T>{
+  Brankas(this.nomorPin,this.nilaiRahasia);
+  String nomorPin;
+  T nilaiRahasia;
+  
+  T ambilNilai(String nomorPin){
+    if(nomorPin == this.nomorPin){
+      return this.nilaiRahasia;
+    }else{
+      return this.nilaiRahasia;
+    }  
+  }
+}
+
+void main() {
+  var brankas1 = Brankas<String?>("123","Ini String Rahasianya");
+  var brankas2 = Brankas<int?>("456",767676);
+  
+  print(brankas1.ambilNilai("123")); //return "Ini String Rahasianya"
+  print(brankas1.ambilNilai("321")); //return null
+  print(brankas2.ambilNilai("456")); //return 767676
+}
